8ff463f0c12650bb0f7ef5e151f69cf5f9597b16
Tau <taujeng.1@gmail.com>
2022-03-18 14:50:09 +0800

    Pop up initial stuff

--- keyboard.js
+++ keyboard.js
@@ -9,6 +9,17 @@ let attempt = 1;
 
 
 
+let popup = document.getElementById("pop1");
+let popup2 = document.getElementById("the_pop")
+popup.addEventListener("click", function() {
+  popup2.classList.toggle("show");
+})
+
+
+
+
+
+
 const Keyboard = {
   // keep track of classes: keyboard, keyboard__keys, the keys
   elements: {
